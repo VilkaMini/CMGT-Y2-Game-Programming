14ed1e74f14f68bd026b5494609e16c407570a9d
Vilius <43584156+VilkaMini@users.noreply.github.com>
2024-01-28 16:00:11 +0100

    Updated Class Diagram

--- Assets/FPS/Scripts/Gameplay/ConstructionObjects/HealthTurretObject.cs
+++ Assets/FPS/Scripts/Gameplay/ConstructionObjects/HealthTurretObject.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using Unity.FPS.Game;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class HealthTurretObject : ConstructionObject
 {
@@ -21,6 +18,11 @@ public class HealthTurretObject : ConstructionObject
     }
     
     void Update()
+    {
+        HealPlayer();
+    }
+
+    void HealPlayer()
     {
         _playerPos = m_ActorsManager.Player.transform.position;
         if (Vector3.Distance(transform.position, _playerPos) < range && _playerHealth.CanPickup())
